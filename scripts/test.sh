2e956a098c3fc61e8c02d9e8f385224177da1d23
Cxx-mlr <spapiernik12@gmail.com>
2024-10-07 12:18:04 -0500

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,5 @@
 set -e
 set -x
 
-uv run --with coverage coverage run -m pytest
+uv run coverage run -m pytest
+uv run coverage report --skip-covered --skip-empty --include=./katty/**/*,./tests/**/*
